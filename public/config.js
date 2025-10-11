4ef7d3b3566becb2cfb6d66ef11f363bf54de125
armelyara <theday.ay@gmail.com>
2025-10-11 09:04:16 -0400

    second commit

--- public/config.js
+++ public/config.js
@@ -1,8 +1,29 @@
 // TraficDay - Secure Configuration
 // This file loads sensitive config from Firebase Remote Config
 
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getRemoteConfig, fetchAndActivate, getString } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-remote-config.js';
-import { app } from './firebase-config.js';
+
+// Firebase config (same as firebase-config.js)
+const firebaseConfig = {
+    apiKey: "AIzaSyB7DwtpC_RggBLkW0w2yHOOGxrXHpyWPfE",
+    authDomain: "traficday-91045.firebaseapp.com",
+    databaseURL: "https://traficday-91045-default-rtdb.firebaseio.com",
+    projectId: "traficday-91045",
+    storageBucket: "traficday-91045.firebasestorage.app",
+    messagingSenderId: "230009461919",
+    appId: "1:230009461919:web:bc09e90d723f66a84556aa"
+};
+
+// Get or initialize Firebase app
+let app;
+try {
+    app = initializeApp(firebaseConfig, 'config-app'); // Use a different name to avoid conflict
+} catch (error) {
+    // If already initialized, get existing app
+    const { getApp } = await import('https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js');
+    app = getApp();
+}
 
 // Initialize Remote Config
 const remoteConfig = getRemoteConfig(app);
